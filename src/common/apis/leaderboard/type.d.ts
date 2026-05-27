2e42261feea5b9a6c63e17e2af30c32643d2f8b1
zhouy <328341003@qq.com>
2026-05-27 14:36:30 +0800

    clone https://github.com/hyhfish/milkonomy

---
+++ src/common/apis/leaderboard/type.d.ts
@@ -0,0 +1,59 @@
+import type { Sort } from "element-plus"
+import type Calculator from "@/calculator"
+
+export interface RequestData {
+  /** 当前页码 */
+  currentPage: number
+  /** 查询条数 */
+  size: number
+  /** 查询参数：名称 */
+  name?: string
+  /** 查询参数：项目 */
+  project?: string
+  /** 查询参数：利润率% */
+  profitRate?: number
+  /** 查询参数：排除装备 */
+  banEquipment?: boolean
+  /** 查询参数：排除护符(charm) */
+  banCharm?: boolean
+  /** 是否计算市场卖出税率(2%)；默认 true */
+  includeTax?: boolean
+  enhanposer?: boolean
+  sort?: Sort
+  actionLevel?: number
+  maxItemLevel?: number
+}
+
+export type ResponseData = ApiResponseData<{
+  list: LeaderboardData[]
+  total: number
+}>
+
+export interface LeaderboardData {
+  calculator: Calculator
+  // todo
+  calculatorList?: Calculator[]
+  resultList?: any[]
+  workMultiplier?: number[]
+
+  hrid: string
+  name: string
+  project: string
+  successRate: number
+  costPH: number
+  consumePH: number
+  gainPH: number
+  incomePH: number
+  profitPH: number
+  profitRate: number
+
+  costPHFormat: string
+  incomePHFormat: string
+  profitPHFormat: string
+  profitPDFormat: string
+  profitRateFormat: string
+  efficiencyFormat: string
+  timeCostFormat: string
+  successRateFormat: string
+
+}
